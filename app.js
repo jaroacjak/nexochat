583c77be0392af22d686f0113d0de27113eea55b
jaroacjak <videojarko@gmail.com>
2026-09-01 10:42:34 +0200

    Initialize NexoChat application with login and register

---
+++ app.js
@@ -0,0 +1,38 @@
+// ==========================================
+// NexoChat - app.js
+// ==========================================
+
+console.log("NexoChat je spustený! 🚀");
+
+// Tlačidlá z index.html
+const loginButton = document.getElementById("loginButton");
+const registerButton = document.getElementById("registerButton");
+
+// Prihlásenie
+loginButton.addEventListener("click", () => {
+    alert("Prihlásenie pripravujeme 🔐");
+});
+
+// Registrácia
+registerButton.addEventListener("click", () => {
+    alert("Registráciu pripravujeme 👤");
+});
+
+
+// ==========================================
+// Neskôr sem pridáme:
+//
+// Appwrite
+// - registrácia
+// - prihlásenie
+// - profily
+// - databázu
+// - správy
+// - obrázky
+// - prílohy
+//
+// WebRTC
+// - hlasové hovory
+// - video hovory
+// - zdieľanie obrazovky
+// ==========================================
